392489cf9b48b1e4ab04373a39c516de938ce2a9
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-05-27 09:04:30 +0100

    Updated README and set docking station maximum capacity

--- Version-2/lib/docking_station.rb
+++ Version-2/lib/docking_station.rb
@@ -1,11 +1,16 @@
 require_relative 'bike'
 class DockingStation
   attr_reader :bike
+
+  def initialize
+    @bikes = []
+  end
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'Sorry, no bikes available' if @bikes.empty?
+    @bikes.pop
   end
   def dock(bike)
-    @bike = bike
+    fail 'You cannot dock a bike, the docking station is full' if @bikes.length >= 20
+    @bikes.push(bike)
   end
 end
